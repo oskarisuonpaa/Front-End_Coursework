505281c332ef2417f388e9d44bd293e9a5bf78a0
oskarisuonpaa <oskari.suonpaa@student.lut.fi>
2022-01-16 02:57:40 +0200

    Updates

---
+++ js/rain.js
@@ -0,0 +1,81 @@
+class Rain {
+    constructor(width, height, direction, posX, posY, speed, color){
+        this.width = width;
+        this.height = height;
+        this.direction = direction;
+        this.posX = posX;
+        this.posY = posY;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    move(){
+        this.posX += Math.sin(this.direction * Math.PI/2)  * this.speed;
+        this.posY += Math.cos(this.direction * Math.PI/2)  * this.speed;
+    }
+
+    draw(){
+        canvasContext.rotate(-this.direction)
+        canvasContext.fillStyle = this.color
+        canvasContext.fillRect(this.posX, this.posY, this.width, this.height)
+        canvasContext.rotate(+this.direction)
+    }
+}
+
+
+let canvas = document.getElementById("background")
+let canvasContext = canvas.getContext('2d');
+
+let createRect = (x,y,width, height,color) => {
+    canvasContext.fillStyle = color
+    canvasContext.fillRect(x,y,width,height)
+}
+
+let allRains = []
+let defaultRainWidth = 1
+let defaultRainHeight = 5
+let maximumRainCount = 500
+
+let maximumRainInitializationInOneFrame = 1
+
+let fps = 60
+let gameLoop = () => {
+    setInterval(show, 1000/fps)
+}
+
+let show = () => {
+    update();
+    draw();
+}
+
+let update = () => {
+    canvasContext.clearRect(0,0, canvas.width, canvas.height)
+    let rainInitCountInThisFrame = 0
+    while(allRains.length < maximumRainCount && maximumRainInitializationInOneFrame > rainInitCountInThisFrame) {
+        let distanceFromCam = Math.random()
+        let rain = new Rain( defaultRainWidth * (2-distanceFromCam)
+                                ,defaultRainHeight* (2-distanceFromCam),
+                                (Math.random()/20),
+                                 Math.random() * canvas.width,
+                                  -100, (2-distanceFromCam) * 2, 
+                                  "rgba(255, 8, 0," + ((1-distanceFromCam ))+ ")")
+        allRains.push(rain);
+        rainInitCountInThisFrame++
+    }
+
+    for(let i = 0; i < allRains.length; i++){
+        allRains[i].move()
+        if(allRains[i].posY > canvas.height - 1 || allRains[i].posX > canvas.width - 1) {
+            allRains.splice(i, 1)
+        }
+    }
+}
+
+let draw = () => {  
+    allRains.forEach(rain=>{ 
+        rain.draw()
+    })
+}
+
+
+gameLoop()
